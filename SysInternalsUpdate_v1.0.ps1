a5b5d79d33310ac784767630641e2c0b6747a05c
joba-tech <57910686+joba-tech@users.noreply.github.com>
2021-03-13 18:25:37 +0100

    Update SysInternalsUpdate_v1.0.ps1

--- SysInternalsUpdate_v1.0.ps1
+++ SysInternalsUpdate_v1.0.ps1
@@ -1,5 +1,5 @@
 ﻿##############################Function##############################
-#             This section contains the used functions             #
+#             This section contains the used functions / Johannes Test             #
 ####################################################################
 
 #region functions
